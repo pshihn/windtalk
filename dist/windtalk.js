c0a12a2c718637d14142af0cd1f0d2b71adacb30
Preet Shihn <shihn@Preets-MacBook-Pro.local>
2018-05-29 15:05:42 -0700

    refactored code for duplex

--- dist/windtalk.js
+++ dist/windtalk.js
@@ -1,76 +1,20 @@
 var windtalk = (function (exports) {
   'use strict';
 
-  function proxy(remote, path) {
-    path = path || [];
-    return new Proxy(function () { }, {
-      get(_, prop, receiver) {
-        if (prop === 'then') {
-          if (path.length === 0) {
-            return { then: () => receiver };
-          }
-          const p = remote.post({ type: 'GET', path });
-          return p.then.bind(p);
-        }
-        return proxy(remote, path.concat(prop));
-      },
-      set(_, prop, value) {
-        return remote.post({ type: 'SET', path: path.concat(prop), value });
-      },
-      apply(_, thisArg, args) {
-        return remote.post({ type: 'APPLY', path, args });
-      }
-    });
-  }
+  let _target, _w, _cbs, _attached = false;
 
-  function randomInt() { return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER); }
+  const _reduce = list => list.reduce((o, prop) => (o ? o[prop] : o), _target);
 
-  class Remote {
-    constructor(w) {
-      this.w = w; // other window
-      this.uid = `${Date.now()}-${randomInt()}`;
-      this.c = 0; // counter
-      this.cbs = {}; // callbacks
-      self.addEventListener('message', event => {
-        const id = event.data && event.data.windtalk && event.data.id;
-        const cb = id && this.cbs[id];
-        if (cb) {
-          delete this.cbs[id];
-          if (event.data.error) {
-            cb[1](new Error(event.data.error));
-          } else {
-            cb[0](event.data.value);
-          }
-        }
-      });
-    }
-    post(request) {
-      const args = request.args || [];
-      const id = `${this.uid}-${++this.c}`;
-      return new Promise((resolve, reject) => {
-        this.cbs[id] = [resolve, reject];
-        this.w.postMessage(Object.assign({}, request, { id, args, target: this.uid, windtalk: true }), '*');
-      });
-    }
-  }
-
-  function link(endPoint) {
-    return proxy(new Remote(endPoint));
-  }
-
-  function expose(target, endPoint) {
-    const _target = target;
-    const w = endPoint || window.top;
-    self.addEventListener('message', async event => {
-      if (event.data && event.data.id && event.data.type && event.data.windtalk) {
-        const data = event.data;
+  async function _handler(event) {
+    const data = event.data;
+    let id, type, cb;
+    if (id = data && data.id) {
+      if ((type = data.type) && _w && _target) {
         data.path = data.path || [];
-        const id = data.id;
-        const reduce = list => list.reduce((o, prop) => (o ? o[prop] : o), _target);
-        const msg = { id, windtalk: true };
-        const ref = reduce(data.path);
-        const refParent = reduce(data.path.slice(0, -1));
-        switch (data.type) {
+        const msg = { id };
+        const ref = _reduce(data.path);
+        const refParent = _reduce(data.path.slice(0, -1));
+        switch (type) {
           case 'GET':
             msg.value = ref;
             break;
@@ -79,7 +23,6 @@ var windtalk = (function (exports) {
             if (prop) {
               refParent[prop] = data.value;
             }
-            msg.value = prop ? true : false;
             break;
           case 'APPLY':
             try {
@@ -89,9 +32,70 @@ var windtalk = (function (exports) {
             }
             break;
         }
-        w.postMessage(msg, '*');
+        _w.postMessage(msg, '*');
+      } else if (_cbs && (cb = _cbs[id])) {
+        delete _cbs[id];
+        if (data.error) {
+          cb[1](new Error(data.error));
+        } else {
+          cb[0](data.value);
+        }
+      }
+    }
+  }
+
+  function _attach() {
+    if (!_attached) {
+      self.addEventListener('message', _handler);
+      _attached = true;
+    }
+  }
+
+  function createRmote(w) {
+    const uid = `${Date.now()}-${Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)}`;
+    let c = 0;
+    _attach();
+    return request => {
+      const args = request.args || [];
+      const id = `${uid}-${++c}`;
+      return new Promise((resolve, reject) => {
+        _cbs[id] = [resolve, reject];
+        w.postMessage(Object.assign({}, request, { id, args }), '*');
+      });
+    };
+  }
+
+  function proxy(remote, path) {
+    path = path || [];
+    return new Proxy(function () { }, {
+      get(_, prop, rec) {
+        if (prop === 'then') {
+          if (path.length === 0) {
+            return { then: () => rec };
+          }
+          const p = remote({ type: 'GET', path });
+          return p.then.bind(p);
+        }
+        return proxy(remote, path.concat(prop));
+      },
+      set(_, prop, value) {
+        return remote({ type: 'SET', path: path.concat(prop), value });
+      },
+      apply(_, thisArg, args) {
+        return remote({ type: 'APPLY', path, args });
       }
     });
+  }
+
+  function link(endPoint) {
+    _cbs = {};
+    return proxy(createRmote(endPoint));
+  }
+
+  function expose(target, endPoint) {
+    _w = endPoint || window.top;
+    _target = target;
+    _attach();
   }
 
   exports.link = link;
