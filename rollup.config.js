a4e28062fc795cdf764a01aced449c24aa148eb1
Preet Shihn <preetshihn@gmail.com>
2019-03-19 00:42:50 -0700

    new build

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import minify from 'rollup-plugin-babel-minify';
+import { terser } from "rollup-plugin-terser";
 
 const input = 'index.js';
 
@@ -6,50 +6,27 @@ export default [
   {
     input,
     output: {
-      file: `dist/windtalk.umd.js`,
-      format: 'umd',
-      name: 'windtalk'
-    }
-  },
-  {
-    input,
-    output: {
-      file: `dist/windtalk.umd.min.js`,
-      format: 'umd',
-      name: 'windtalk'
-    },
-    plugins: [minify({ comments: false })]
-  },
-  {
-    input,
-    output: {
       file: `dist/windtalk.js`,
       format: 'iife',
       name: 'windtalk'
-    }
+    },
+    plugins: [terser()]
   },
   {
     input,
     output: {
-      file: `dist/windtalk.min.js`,
-      format: 'iife',
+      file: `dist/windtalk.umd.js`,
+      format: 'umd',
       name: 'windtalk'
     },
-    plugins: [minify({ comments: false })]
+    plugins: [terser()]
   },
   {
     input,
     output: {
       file: `dist/windtalk.m.js`,
       format: 'esm'
-    }
-  },
-  {
-    input,
-    output: {
-      file: `dist/windtalk.m.min.js`,
-      format: 'esm'
     },
-    plugins: [minify({ comments: false })]
+    plugins: [terser()]
   },
 ];
